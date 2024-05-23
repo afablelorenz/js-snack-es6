cb3cb10a2975080d95821ed81f775e4122a0beeb
afablelorenz <afablelorenz@gmail.com>
2024-05-23 16:55:34 +0200

    Snack 1 updated function

--- snack-1/js/script.js
+++ snack-1/js/script.js
@@ -1,8 +1,15 @@
 const vip = [ 'Brad Pitt', 'Johnny Depp', 'Lady Gaga', 'Cristiano Ronaldo', 'Georgina Rodriguez', 'Chiara Ferragni',  'George Clooney', 'Amal Clooney', 'Fedez', 'Amadeus', 'Fiorello'];
 
 
-let vipTavolo = vip.map((element) => {
-    return {guestname: element};
+let vipTavolo = vip.map((element,i) => {
+    return {
+        tableName: 'Tavolo Vip',
+        guestName: element,
+        place: i,
+    };
 });
+
+
+
 
 console.log(vipTavolo)
