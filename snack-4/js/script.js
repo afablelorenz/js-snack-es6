ed26eef70f2dd5d01b2450ae00c39d851fafd00d
afablelorenz <afablelorenz@gmail.com>
2024-05-26 22:02:40 +0200

    snack 4 - created squadre array

---
+++ snack-4/js/script.js
@@ -0,0 +1,28 @@
+const squadre = [
+    {
+        nome: 'squadra1',
+        puntiFatti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome: 'squadra2',
+        puntiFatti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome: 'squadra3',
+        puntiFatti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome: 'squadra4',
+        puntiFatti: 0,
+        falliSubiti:0,
+    },
+    {
+        nome: 'squadra5',
+        puntiFatti: 0,
+        falliSubiti:0,
+    },
+];
+
