5fafa64912180d3cb864a635f93688f361fab50e
afablelorenz <afablelorenz@gmail.com>
2024-05-26 19:37:59 +0200

    snack 3 - print bike with minimum weight

---
+++ snack-3/js/script.js
@@ -0,0 +1,26 @@
+const bici = [
+    {
+        nome:'bici1',
+        peso:7.2,
+    },
+    {
+        nome:'bici2',
+        peso:7.7,
+    },
+    {
+        nome:'bici3',
+        peso:7.1,
+    },
+    {
+        nome:'bici4',
+        peso:6.2,
+    },
+    {
+        nome:'bici5',
+        peso:8.2,
+    },
+];
+
+const minBiciPeso = Math.min(...bici.map(bici => bici.peso));
+
+console.log(minBiciPeso);
